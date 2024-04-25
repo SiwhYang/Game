1b3341adcf230b3c9aa0d6d0488f711940badcf7
WillyYang <willy.yang@quantatw.com>
2024-04-25 16:52:07 +0800

    adding the function of clicking myself

--- test.py
+++ test.py
@@ -1,7 +1,16 @@
 import pydirectinput
 import time
 
-while (True):
-    pydirectinput.press('F1')
-    time.sleep(1)
-    pydirectinput.keyUp('F1')
+def string_spliter(string):
+    string_list = []
+    splitat = int(len(string)/3)
+    left ,right = string[:splitat], string[splitat+splitat:]
+    middle = string [splitat :-splitat]
+    string_list.append(left)
+    string_list.append(middle)
+    string_list.append(right)
+    return string_list
+print(string_spliter("Palser Beta"))
+# string_spliter("Assasin Builder A")
+# string_spliter("RedDust")
+# string_spliter("Palser Beta")
